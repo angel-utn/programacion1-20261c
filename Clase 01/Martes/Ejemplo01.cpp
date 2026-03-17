bba27da7cceddadb555a0355e235735c1f5663a1
Angel <asimon@docentes.frgp.utn.edu.ar>
2026-03-17 20:44:10 -0300

    Archivos de clase 01 - martes

---
+++ Clase 01/Martes/Ejemplo01.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int num1, num2, resultado;
+
+  cout << "Ingresar primer número: ";
+  cin >> num1;
+  cout << "Ingresar segundo número: ";
+  cin >> num2;
+  resultado = num1 + num2;
+
+  // Comentario de una linea
+  /*
+  cout << endl;
+  cout << "Resultado de la suma: ";
+  cout << resultado;
+  */
+  cout << endl << "Resultado de la suma: " << resultado << endl;
+
+  return 0;
+}
